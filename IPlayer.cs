a1b475a652ea39f87c59589caa42b0fc97d554fe
Raiju-Deeq <mdeeq0@me.com>
2025-03-06 10:51:35 +0000

    Fixed Powerups

--- IPlayer.cs
+++ IPlayer.cs
@@ -1,39 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GamePlayDLL
+﻿namespace GamePlayDLL
 {
-    /// <summary>
-    /// Defines the interface for the player in the game.
-    /// </summary>
     public interface IPlayer
     {
-        /// <summary>
-        /// Applies a power-up effect to the player.
-        /// </summary>
-        /// <param name="type">The type of power-up.</param>
-        /// <param name="duration">The duration of the power-up effect.</param>
-        void ApplyPowerUp(PowerUpType type, float duration);
-
-        /// <summary>
-        /// Applies damage to the player.
-        /// </summary>
-        /// <param name="damage">The amount of damage to apply.</param>
-        void TakeDamage(int damage);
-
-        /// <summary>
-        /// Adds score to the player's total.
-        /// </summary>
-        /// <param name="points">The number of points to add.</param>
+        float GetMovementSpeed();
+        void SetMovementSpeed(float speed);
+        bool IsInvulnerable();
+        void SetInvulnerable(bool invulnerable);
+        int GetScoreMultiplier();
+        void SetScoreMultiplier(int multiplier);
         void AddScore(int points);
-
-        /// <summary>
-        /// Gets the player's current score.
-        /// </summary>
-        /// <returns>The current score.</returns>
         int GetScore();
+        void TakeDamage(int damage); // Add this method for handling damage
     }
 }
---
+++ PowerUpEffect.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamePlayDLL
+{
+    public abstract class PowerUpEffect
+    {
+        public abstract void ApplyEffect(IPlayer player);
+        public abstract void RemoveEffect(IPlayer player);
+    }
+
+    public class SpeedBoostEffect : PowerUpEffect
+    {
+        public override void ApplyEffect(IPlayer player)
+        {
+            player.SetMovementSpeed(player.GetMovementSpeed() * 1.5f);
+        }
+
+        public override void RemoveEffect(IPlayer player)
+        {
+            player.SetMovementSpeed(player.GetMovementSpeed() / 1.5f);
+        }
+    }
+
+    public class ShieldEffect : PowerUpEffect
+    {
+        public override void ApplyEffect(IPlayer player)
+        {
+            player.SetInvulnerable(true);
+        }
+
+        public override void RemoveEffect(IPlayer player)
+        {
+            player.SetInvulnerable(false);
+        }
+    }
+
+    public class DoublePointsEffect : PowerUpEffect
+    {
+        public override void ApplyEffect(IPlayer player)
+        {
+            player.SetScoreMultiplier(2);
+        }
+
+        public override void RemoveEffect(IPlayer player)
+        {
+            player.SetScoreMultiplier(1);
+        }
+    }
+
+    public class CoinEffect : PowerUpEffect
+    {
+        public override void ApplyEffect(IPlayer player)
+        {
+            player.AddScore(100);
+        }
+
+        public override void RemoveEffect(IPlayer player)
+        {
+            // No need to remove effect for instant power-ups
+        }
+    }
+
+}
